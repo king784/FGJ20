944c951e4b63cf92252faaae109bb236eac97202
Milo <45202726+Milo-Hakalahti@users.noreply.github.com>
2020-02-01 00:59:20 +0200

    Milon tekemät muutokset
    asd

---
+++ Assets/enterNameScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class enterNameScript : MonoBehaviour
+{
+    public Button enterNameButton;
+    public TextMeshProUGUI textmeshproinput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enterNameButton.onClick.AddListener(TestName);
+    }
+
+    void TestName()
+    {
+        if (textmeshproinput.text.CompareTo("your name") == 1)
+        {
+            Debug.Log(textmeshproinput.text + " is right");
+        }
+        else Debug.Log("fuck you " + textmeshproinput.text);
+    }
+}
---
+++ Assets/typeyournameScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class typeyournameScript : MonoBehaviour
+{
+    public TextMeshProUGUI textmeshproinput;   
+    
+    void Start()
+    {
+        
+    }
+
+    public void EnterName(TextMeshProUGUI name)
+    {
+        name.text = textmeshproinput.text;
+
+        if (name.text != "your name")
+        {
+            Debug.Log("not your name! Try again");
+        }
+        else
+            Debug.Log("Correct!");
+    }
+}
