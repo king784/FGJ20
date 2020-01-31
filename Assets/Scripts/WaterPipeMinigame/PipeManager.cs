7c26efc1dc76cf9ca8f5bf60c44749aa1249cd35
Teemu Turku <teemu.turku94@gmail.com>
2020-01-31 21:16:00 +0200

    pipes test

---
+++ Assets/Scripts/WaterPipeMinigame/PipeManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PipeManager : MonoBehaviour
+{
+    [SerializeField]
+    int width;
+    [SerializeField]
+    int height;
+    [SerializeField]
+    GameObject gridObj;
+    [SerializeField]
+    GridLayoutGroup grid;
+
+    [ContextMenu("Test")]
+    void SpawnGrid()
+    {
+        grid.constraintCount = height;
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                Instantiate(gridObj, grid.transform);
+            }
+        }
+        Vector2 topLeft;
+    }
+}
