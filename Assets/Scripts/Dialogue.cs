67526d27b0375deb56704325068433377e3e67d8
Milo <45202726+Milo-Hakalahti@users.noreply.github.com>
2020-02-01 17:43:35 +0200

    type your name

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    [TextArea(3, 10)]
+    public string[] sentences;
+}
+
+[System.Serializable]
+public class WrongDialogue
+{
+    [TextArea(3, 10)]
+    public string[] wrongSentences;
+}
