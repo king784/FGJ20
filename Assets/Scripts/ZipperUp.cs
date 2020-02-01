2ac3c989845fc856a57ff827926542f0a90044c9
Teemu Turku <teemu.turku94@gmail.com>
2020-02-01 22:58:19 +0200

    Update ZipperUp.cs

--- Assets/Scripts/ZipperUp.cs
+++ Assets/Scripts/ZipperUp.cs
@@ -21,6 +21,7 @@ public class ZipperUp : MonoBehaviour
         {
             victoryPanel.SetActive(true);
             slider.enabled = false;
+            return;
         }
 
         picturePanels[(int)slider.value].SetActive(true);
