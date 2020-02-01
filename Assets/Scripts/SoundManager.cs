af9ee69d68305fd2c618d59e06e190a8ab0f4975
TaraTorpeedo <54583576+TaraTorpeedo@users.noreply.github.com>
2020-02-01 19:47:34 +0200

    audiomanager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource effectSource;                    //For effects
+    public AudioSource musicSource;                  //For music
+    public static SoundManager instance = null;        
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySingle(AudioClip clip)
+    {
+        effectSource.clip = clip;
+
+        effectSource.Play();
+    }
+}
