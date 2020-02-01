7b715b2ea93081b9003d377ef03e17abe1701e05
Janessa <s00190968@mail.itsligo.ie>
2020-02-01 15:38:37 +0200

    edited main menu and added scene change from glitches

---
+++ Assets/Scripts/GlitchChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlitchChangeScene : MonoBehaviour
+{
+
+    public string worldToGoto = "";
+
+    public void moveToGlitchedWorld()
+    {
+        SceneManager.LoadScene(worldToGoto);
+    }
+}
