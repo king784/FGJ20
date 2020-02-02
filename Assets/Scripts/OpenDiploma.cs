f592a50602072be11f94b8969b35aee9f7bfa8e5
Milo <45202726+Milo-Hakalahti@users.noreply.github.com>
2020-02-02 12:13:10 +0200

    End game screen
    this

---
+++ Assets/Scripts/OpenDiploma.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenDiploma : MonoBehaviour
+{
+    public Button openDiplomaBtn;
+    public GameObject firstCanvas;
+    public GameObject secondCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        openDiplomaBtn.onClick.AddListener(OpenDiplomaFile);   
+    }
+
+    public void OpenDiplomaFile()
+    {
+        firstCanvas.SetActive(false);
+        secondCanvas.SetActive(true);
+    }
+}
