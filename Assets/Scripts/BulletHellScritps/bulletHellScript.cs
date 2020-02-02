c4fbef46f950729ea36e2f41c3c0d07356876d44
Teemu Turku <teemu.turku94@gmail.com>
2020-02-02 11:54:11 +0200

    advanced moras ai

--- Assets/Scripts/BulletHellScritps/bulletHellScript.cs
+++ Assets/Scripts/BulletHellScritps/bulletHellScript.cs
@@ -9,10 +9,11 @@ public class bulletHellScript : MonoBehaviour
     public GameObject rEye;
     public GameObject moras;
     public GameObject bullet;
+    bool shooting = true;
 
     void Start()
     {
-        StartCoroutine(ShootAround());
+        StartCoroutine(ShootRoutine());
     }
     
     void Update()
@@ -28,14 +29,37 @@ public class bulletHellScript : MonoBehaviour
         }
     }
 
-    IEnumerator ShootAround()
+    IEnumerator ShootRoutine()
+    {
+        while(shooting)
+        {
+            yield return new WaitForSeconds(Random.Range(3.0f, 6.0f));
+            StartCoroutine(ShootLeftEyeAround());
+            StartCoroutine(ShootRightEyeAround());
+        }
+    }
+
+    IEnumerator ShootLeftEyeAround()
     {
         float a = 360/100;
         Vector3 morasUp = moras.transform.up;
-        for(int i = 0; i < 100; i++)
+        for(int i = 0; i < 50; i++)
         {
             morasUp = Quaternion.Euler(0, 0, a*i) * morasUp;
             GameObject obj = Instantiate(bullet, lEye.transform.position, Quaternion.identity);
+            obj.GetComponent<Rigidbody2D>().AddForce(morasUp * 5.0f, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator ShootRightEyeAround()
+    {
+        float a = 360/100;
+        Vector3 morasUp = moras.transform.up;
+        for(int i = 0; i < 50; i++)
+        {
+            morasUp = Quaternion.Euler(0, 0, a*i) * morasUp;
+            GameObject obj = Instantiate(bullet, rEye.transform.position, Quaternion.identity);
             obj.GetComponent<Rigidbody2D>().AddForce(morasUp * 5.0f, ForceMode2D.Impulse);
             yield return new WaitForSeconds(0.05f);
         }
